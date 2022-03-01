9b95fb7109938ac6919ea72d78ea787d21068134
islam <islamabdallah88@gmail.com>
2022-03-01 15:48:56 +0200

    Update Api

---
+++ TireManagment/Api/AccountApiController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TireManagment.DbModels;
+using TireManagment.Enums;
+using TireManagment.Models;
+using TireManagment.Services;
+
+namespace TireManagment.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountApiController : ControllerBase
+    {
+        AccountService AccountService;
+        SignInManager<AppUser> SignInManger;
+        UserManager<AppUser> UserManger;
+
+        public AccountApiController(AccountService _accountService, SignInManager<AppUser> _signInManger, UserManager<AppUser> _UserManager)
+        {
+            AccountService = _accountService;
+            SignInManger = _signInManger;
+            UserManger = _UserManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            var result = await AccountService.Register(registerViewModel, Role.TireMan);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            var _user = await UserManger.FindByEmailAsync(loginViewModel.email);
+            if (_user != null)
+            {
+                var _result = await SignInManger.PasswordSignInAsync(_user.UserName, loginViewModel.password, loginViewModel.RememberMe, true);
+
+                if (_result.Succeeded)
+                {
+                    return Ok(new { Flag = true, Message = "Done", Data = _result });
+                }
+                return BadRequest(new { Flag = false, Message = "Error , Password Not Correct", Data = 0 }); 
+            }
+            return BadRequest(new { Flag = false, Message = "Error , Email Not Exist", Data = 0 });
+        }
+    }
+}
---
+++ TireManagment/Api/TruckApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TireManagment.Services;
+
+namespace TireManagment.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TruckApiController : ControllerBase
+    {
+        TruckService TruckService;
+        CategoryService CategoryService;
+
+        public TruckApiController(TruckService truck, CategoryService _categoryService)
+        {
+            TruckService = truck;
+            CategoryService = _categoryService;
+
+        }
+
+        [HttpGet("TruckList")]
+        public IActionResult Trucks()
+        {
+            var _trucks = TruckService.GetAll();
+            if (_trucks != null)
+                return Ok(new { Flag = true, Message = "Done", Data = _trucks });
+            else
+                return BadRequest(new { Flag = false, Message = "Error", Data = 0 });
+        }
+    }
+}
